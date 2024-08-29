bc8f733980f2c842835b70e3913bd9a7dbebda38
simonqq21 <ques86225@gmail.com>
2024-08-29 23:51:54 +0800

    python test sequence program confirms that the websocket module is working properly without bugs or crashes.

--- ws_testing/test_sequence.py
+++ ws_testing/test_sequence.py
@@ -73,13 +73,13 @@ class Client():
                 pass
 
     async def loadWifis(self):
-        print("Testing ESP32 websockets load wifis")
+        print("load wifis")
         # list wifis
         data = {}
         data["cmd"] = "request"
         data["type"] = "wifis"
         data["payload"] = {}
-        websocket.send(json.dumps(data))
+        await self.websocket.send(json.dumps(data))
 
     async def loadConnection(self):
         print("load connection")
@@ -90,99 +90,174 @@ class Client():
         await self.websocket.send(json.dumps(data))
 
     async def loadDateTime(self):
-        print("Testing ESP32 websockets load datetime")
+        print("load datetime")
         data = {}
         data["cmd"] = "request"
         data["type"] = "datetime"
         data["payload"] = {}
-        websocket.send(json.dumps(data))
+        await self.websocket.send(json.dumps(data))
 
     async def loadRelayState(self):
-        print("Testing ESP32 websockets load relay_state")
+        print("load relay_state")
         data = {}
         data["cmd"] = "request"
         data["type"] = "relay_state"
         data["payload"] = {}
-        websocket.send(json.dumps(data))
+        await self.websocket.send(json.dumps(data))
 
     async def loadConfig(self):
-        print("Testing ESP32 websockets load config")
+        print("load config")
         data = {}
         data["cmd"] = "request"
         data["type"] = "config"
         data["payload"] = {}
-        websocket.send(json.dumps(data))
+        await self.websocket.send(json.dumps(data))
 
-    async def saveConnection(self):
+    async def saveConnection(self, ssid, password, ipIndex, port):
+        print("save connection")
         data = {}
         data["cmd"] = "save"
         data["type"] = "connection"
         data["payload"] = {}
-        data["payload"]["ssid"] = "QUE-STARLINK"
-        data["payload"]["pass"] = "Quefamily01259"
-        data["payload"]["ipIndex"] = 70
-        data["payload"]["port"] = 5555
+        data["payload"]["ssid"] = ssid
+        data["payload"]["pass"] = password
+        data["payload"]["ipIndex"] = ipIndex
+        data["payload"]["port"] = port
         # save connection
-        websocket.send(json.dumps(data))
+        await self.websocket.send(json.dumps(data))
 
-    async def saveDateTime(self):
-        print("Testing ESP32 websockets save datetime")
+    async def saveDateTime(self, datetimeObj):
+        print("save datetime")
         data = {}
         data["cmd"] = "save"
         data["type"] = "datetime"
         data["payload"] = {}
-        data["payload"]["datetime"] = datetime.now().isoformat()
-        websocket.send(json.dumps(data))
+        data["payload"]["datetime"] = datetimeObj.isoformat()
+        await self.websocket.send(json.dumps(data))
         print("Saved datetime to ESP32.")
 
-    async def saveRelayState(self):
-        print("Testing ESP32 websockets save relay_state")
+    async def saveRelayState(self, relay_state):
+        print("save relay_state")
         data = {}
         data["cmd"] = "save"
         data["type"] = "relay_state"
         data["payload"] = {}
-        data["payload"]["relay_state"] = True
-        websocket.send(json.dumps(data))
+        data["payload"]["relay_state"] = relay_state
+        await self.websocket.send(json.dumps(data))
         print("Saved relay_state to ESP32.")
 
-    async def saveConfig(self):
-        print("Testing ESP32 websockets save config")
-        print("Test save configuration info to ESP32")
+    # timeSlots is list of dicts with format:
+    # {
+    #     "index": int,
+    #     "onStartTime": datetime.time,
+    #     "onEndTime": datetime.time,
+    #     "enabled": bool
+    # }
+    async def saveConfig(self, name, ntpEnabledSetting, gmtOffsetSetting, timerEnabledSetting, \
+                         ledSetting, timeSlots):
+        print("save config")
         data = {}
         data["cmd"] = "save"
         data["type"] = "config"
         data["payload"] = {}
-        data["payload"]["name"] = "basil_ESP32"
-        data["payload"]["ntpEnabledSetting"] = True 
-        data["payload"]["gmtOffsetSetting"] = 8
-        data["payload"]["timerEnabledSetting"] = True
-        data["payload"]["ledSetting"] = 1
-        data["payload"]["timeSlots"] = []
-        timeslot_times = [{"onStartTime": time(6,0,0),
-                           "onEndTime": time(6,0,30)}, 
-                           {"onStartTime": time(12,0,0),
-                           "onEndTime": time(14,0,0)}, 
-                           {"onStartTime": time(18,0,0),
-                           "onEndTime": time(21,0,0)}, ]
-        for i in range(3):
-            timeslot = {}
-            timeslot["index"] = i 
-            timeslot["enabled"] = True 
-            timeslot["onStartTime"] = timeslot_times[i]["onStartTime"].isoformat()
-            timeslot["onEndTime"] = timeslot_times[i]["onEndTime"].isoformat()
-            data["payload"]["timeSlots"].append(timeslot)
-        websocket.send(json.dumps(data))
-        print("Saved config to ESP32.")
+        data["payload"]["name"] = name
+        data["payload"]["ntpEnabledSetting"] = ntpEnabledSetting 
+        data["payload"]["gmtOffsetSetting"] = gmtOffsetSetting
+        data["payload"]["timerEnabledSetting"] = timerEnabledSetting
+        data["payload"]["ledSetting"] = ledSetting
+        data["payload"]["timeSlots"] = timeSlots
+        for i in range(len(timeSlots)):
+            data["payload"]["timeSlots"][i]["onStartTime"] = data["payload"]["timeSlots"][i]["onStartTime"].isoformat()
+            data["payload"]["timeSlots"][i]["onEndTime"] = data["payload"]["timeSlots"][i]["onEndTime"].isoformat()
+        await self.websocket.send(json.dumps(data))
 
-async def testSequence(client):
+async def testSequence(client: Client, apMode):
+    print("ESP32 websocket module testing sequence")
+    await client.loadWifis()
+    await asyncio.sleep(3)
     await client.loadConnection()
+    await asyncio.sleep(3)
+    await client.loadDateTime()
+    await asyncio.sleep(3)
+    await client.loadRelayState()
+    await asyncio.sleep(3)
+    await client.loadConfig()
+    await asyncio.sleep(3)
+
+    await client.saveDateTime(datetime(2024,1,2,7,0,0))
+    await client.loadDateTime()
+    await asyncio.sleep(3)
+    await client.saveRelayState(True)
+    await client.loadRelayState()
+    await asyncio.sleep(3)
+    await client.saveConfig("mint", True, 8, True, 1, [
+        {
+            "index": 0,
+            "onStartTime": time(6,0,0),
+            "onEndTime": time(8,0,0),
+            "enabled": True
+        },
+        {
+            "index": 1,
+            "onStartTime": time(8,0,0),
+            "onEndTime": time(9,0,0),
+            "enabled": True
+        },
+        {
+            "index": 2,
+            "onStartTime": time(10,0,0),
+            "onEndTime": time(15,0,0),
+            "enabled": False
+        }
+    ])
+    await client.loadConfig()
+    await asyncio.sleep(3)
+
+    await client.saveDateTime(datetime.now())
+    await client.loadDateTime()
+    await asyncio.sleep(3)
+    await client.saveRelayState(False)
+    await client.loadRelayState()
+    await asyncio.sleep(3)
+    await client.saveConfig("basil", True, 8, False, 2, [
+        {
+            "index": 0,
+            "onStartTime": time(6,0,0),
+            "onEndTime": time(8,0,0),
+            "enabled": False
+        },
+        {
+            "index": 1,
+            "onStartTime": time(8,0,0),
+            "onEndTime": time(9,0,0),
+            "enabled": False
+        },
+        {
+            "index": 2,
+            "onStartTime": time(9,0,0),
+            "onEndTime": time(10,0,0),
+            "enabled": False
+        }
+    ])
+    await client.loadConfig()
+    await asyncio.sleep(3)
+
+    if (apMode):
+        await client.saveConnection("QUE-STARLINK", "Quefamily01259", 70, 5555)
+    else:
+        await client.saveConnection("QUE-STARLINK", "wrongpassword", 70, 5555)
 
 async def main():
-    client = Client("192.168.5.70", 5555, "ws")
+    apMode = True 
+    # apMode = False
+    if (apMode):
+        client = Client("192.168.4.1", 5555, "ws")
+    else:
+        client = Client("192.168.5.70", 5555, "ws")
     await client.startWS()
     # task1 = asyncio.create_task(func1())
     receiveMessageTask = asyncio.create_task(client.receiveWSMessages())
-    testSequenceTask = asyncio.create_task(testSequence(client))
+    testSequenceTask = asyncio.create_task(testSequence(client, apMode))
     await asyncio.gather(receiveMessageTask, testSequenceTask)
 
 if __name__ == "__main__":
